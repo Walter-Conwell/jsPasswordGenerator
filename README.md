6683e9b339ec5de3f13525d68443461fdb38c897
Walter-Conwell <131832544+Walter-Conwell@users.noreply.github.com>
2023-08-10 11:09:38 -0600

    fixed styling, style structure, added clarifying comments, and cleaned up code. Added future development plan as well.

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
 # jsPasswordGenerator
 
+## Description:
+
+In this fast-paced world, save time by using this password generator!. "Press the generate" button, and then answer a series of prompts. Once you answer those prompts, a new password will be generated and displayed within the read-only text area. Before generating a new password, refresh the page.
+
+## Screenshot:
+
+![passwordGenStart](./assets/images/image.png)
+
+![prompts](./assets/images/prompt.PNG)
+
+![newPassword](./assets/images/image-2.png)
+
 ## Link to deployed application:
 
 https://walter-conwell.github.io/jsPasswordGenerator/
@@ -8,50 +20,7 @@ https://walter-conwell.github.io/jsPasswordGenerator/
 
 https://github.com/Walter-Conwell/jsPasswordGenerator
 
-## Description:
-
-The devlop folder contains the source code, whereas the develop.me folder contains the refactored code.
-Select the criteria.Btn's in order to select or deselect criteria. The generated password will consider the criteria and return accordingly.
-
-## Screenshot:
-
-![jsPasswordGenScreenshot](image-1.png)
-
-## Task:
-
-refactor. This week's Challenge requires you to modify starter code to create an application that enables employees to generate random passwords based on criteria that they’ve selected. This app will run in the browser and will feature dynamically updated HTML and CSS powered by JavaScript code that you write. It will have a clean and polished, responsive user interface that adapts to multiple screen sizes. Note - if all criteriaBtns are pressed,the generateBtn will return undefined.
-
-The password can include special characters. If you’re unfamiliar with these, see this [list of password special characters](https://www.owasp.org/index.php/Password_special_characters) from the OWASP Foundation.
-
-## User Story:
-
-```
-AS AN employee with access to sensitive data
-I WANT to randomly generate a password that meets certain criteria
-SO THAT I can create a strong password that provides greater security
-```
-
-## Acceptance Criteria:
-
-```
-GIVEN I need a new, secure password
-WHEN I click the button to generate a password
-THEN I am presented with a series of prompts for password criteria
-WHEN prompted for password criteria
-THEN I select which criteria to include in the password
-WHEN prompted for the length of the password
-THEN I choose a length of at least 8 characters and no more than 128 characters
-WHEN asked for character types to include in the password
-THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-WHEN I answer each prompt
-THEN my input should be validated and at least one character type should be selected
-WHEN all prompts are answered
-THEN a password is generated that matches the selected criteria
-WHEN the password is generated
-THEN the password is either displayed in an alert or written to the page
-```
-
-## Credits/Sources:
+### Sources:
 
 ```
 - Jacob Carver (tutoring)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,9 @@
-//  to do (to fix)
-// 5. fix file structure
-// 6. put screenshot, live link, and description in readme.
-// 7. make sure password displays properly.
-// error -> after one password is generated, new password is just concatenated on. clear local storage, on regenerate?
+// (fix)
+// error -> after one password is generated, new password is just concatenated on.
+// add visual flare, and explore icebox for features (ie save) or i may do a new version that has a built in password manager...
 
-
-// Assignment Code
 // variable declarations
+// empty strings so that user can confirm whether criteria inclusion is true or false (boolean)
 let lengthConfirm = "";
 let specCharsConfirm;
 let lowCaseConfirm;
@@ -105,6 +102,7 @@ let digitCriteria = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 // function: displays prompt for user input.
 function generatePassword() {
   lengthConfirm = prompt("Enter Your Desired Password Length...");
+  // length range extended by two numbers here (7-129) because 8-128 would exclude those respective numbers.
   while (lengthConfirm <= 7 || lengthConfirm >= 129) {
     alert("The Password Length MUST be within 8-128 Characters!");
     console.log("alert working");
@@ -123,20 +121,26 @@ function generatePassword() {
   );
   let digitConfirm = confirm("Hit OK To Allow Digits In Your Password");
   // error -> when all criteriaConfirm are set to cancel, undefined is returned.
-while(lowCaseConfirm === false && digitConfirm === false && upCaseConfirm === false && specCharsConfirm === false) {
-  alert("At least one rule is needed for a new password.")
-  let specCharsConfirm = confirm(
-    "Hit OK To Allow Special Characters In Your Password"
-  );
-  let lowCaseConfirm = confirm(
-    "Hit OK To Allow Lower Case Characters In Your Password"
-  );
-  let upCaseConfirm = confirm(
-    "Hit OK To Allow Upper Case Characters In Your Password"
-  );
-  let digitConfirm = confirm("Hit OK To Allow Digits In Your Password");
-}
-  // if confirm variables are true ^, concatenate confirm arrays into the new password to be displayed (which is a string)
+  // fixed below!
+  while (
+    lowCaseConfirm === false &&
+    digitConfirm === false &&
+    upCaseConfirm === false &&
+    specCharsConfirm === false
+  ) {
+    alert("At least one rule is needed for a new password.");
+    let specCharsConfirm = confirm(
+      "Hit OK To Allow Special Characters In Your Password"
+    );
+    let lowCaseConfirm = confirm(
+      "Hit OK To Allow Lower Case Characters In Your Password"
+    );
+    let upCaseConfirm = confirm(
+      "Hit OK To Allow Upper Case Characters In Your Password"
+    );
+    let digitConfirm = confirm("Hit OK To Allow Digits In Your Password");
+  }
+  // if any confirm variables are true ^, concatenate them into the new password to be displayed (a string)
   if (specCharsConfirm) {
     generatedChars = generatedChars.concat(specCharsCriteria);
   }
@@ -158,7 +162,7 @@ while(lowCaseConfirm === false && digitConfirm === false && upCaseConfirm === fa
   return newPassword;
 }
 
-// Write password to the #password input
+// place password within the text area.
 function writePassword() {
   const password = generatePassword();
   var passwordText = document.querySelector("#newPassword");
@@ -168,6 +172,3 @@ function writePassword() {
 
 // event listener for generate button.
 document.querySelector("#generate").addEventListener("click", writePassword);
-
-// progress - for version control..
-// currently working. Will clean up code for readability, and will add some styling flare next.
